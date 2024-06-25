066f613f093aaa62a68dddf5238135e684b78be1
Vara Prasad Velpula <varaprasadvelpula7299@gmail.com>
2024-06-26 01:00:53 +0530

    Created starting page, slider and categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">helo</div>;
+  return <Home />;
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+  {
+    id: 0,
+    img: "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525084428e1xcb.png",
+    title: "SUMMER SALE",
+    desc: "DONT COMPROMISE GET FLAT 30% OFF FOR SUMMER SALE",
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525084428e1xcb.png",
+    title: "WINTER SALE",
+    desc: "DONT COMPROMISE GET FLAT 30% OFF FOR WINTER SALE",
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://purepng.com/public/uploads/large/purepng.com-women-shoppingwomenpeoplepersonsfemaleshopping-1121525084428e1xcb.png",
+    title: "POPULAR SALE",
+    desc: "DONT COMPROMISE GET FLAT 30% OFF FOR POPULAR SALE",
+    bg: "fef0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://connecteam.com/wp-content/uploads/2019/03/Screen-Shot-2019-03-20-at-13.22.36.png",
+    title: "Business Attire",
+  },
+  {
+    id: 2,
+    img: "https://imageio.forbes.com/specials-images/imageserve/646c145862a8991d9cb502ff/Meghan-M--Biro-on-how-to-dress-smart-casual-/960x0.jpg?format=jpg&width=960",
+    title: "Casual Wear",
+  },
+  {
+    id: 3,
+    img: "https://qph.cf2.quoracdn.net/main-qimg-c1b1ab7b24d0ef39f91ce583591a7c3b.webp",
+    title: "Formal Wear",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+  {
+    id: 1,
+    img: "",
+  },
+];
